10dc0d2e340f7d87b0c9a8e094dbdbe0c8c79e28
Aryan Majeed <aryanmajid97@gmail.com>
2021-09-06 00:42:24 +0300

    fix(Links): fix links in MoviesDisplay

--- src/components/MoviesDisplay.jsx
+++ src/components/MoviesDisplay.jsx
@@ -10,14 +10,14 @@ function MoviesDisplay({ movie }) {
 		<Col lg={3} md={4} sm={6} xs={12}>
 			<Link to={`/movie/${movie.id}`}>
 				<Poster img={movie.poster_path} />
-				<MovieDetail
-					id={movie.id}
-					title={movie.title}
-					releaseDate={movie.release_date}
-					voteAverage={movie.vote_average}
-					originalLanguage={movie.original_language}
-				/>
 			</Link>
+			<MovieDetail
+				id={movie.id}
+				title={movie.title}
+				releaseDate={movie.release_date}
+				voteAverage={movie.vote_average}
+				originalLanguage={movie.original_language}
+			/>
 		</Col>
 	);
 }
