785bfa9243ed66451240499fd4f3e60319496bcd
bnar98 <bnar.paskandy@gmail.com>
2021-09-05 21:10:30 -0700

    change some styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Container, Row, Col } from 'react-bootstrap';
 import HomePage from './pages/home/HomePage';
 import Movies from './pages/Movies';
 import Movie from './pages/Movie';
@@ -16,35 +15,22 @@ import Footer from './components/Footer';
 
 function App() {
 	return (
-		<div className="App">
+		<div className="">
 			<BrowserRouter>
 				<Header />
-				<Container>
-					<Row>
-						<Col>
-							<Header />
-						</Col>
-					</Row>
-					<Row>
-						<Col>
-							<Switch>
-								<Route exact path="/" component={HomePage} />
-								<Route exact path="/movies" component={Movies} />
-								<Route exact path="/movie/:id" component={Movie} />
-								<Route exact path="/actors" component={Actors} />
-								<Route exact path="/actor/:id" component={Actor} />
-								<Route exact path="/bookmarks" component={Bookmarks} />
-								<Route exact path="/signup" component={Signup} />
-								<Route exact path="/login" component={Signin} />
-							</Switch>
-						</Col>
-					</Row>
-					<Row>
-						<Col>
-							<Footer />
-						</Col>
-					</Row>
-				</Container>
+
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route exact path="/movies" component={Movies} />
+					<Route exact path="/movie/:id" component={Movie} />
+					<Route exact path="/actors" component={Actors} />
+					<Route exact path="/actor/:id" component={Actor} />
+					<Route exact path="/bookmarks" component={Bookmarks} />
+					<Route exact path="/signup" component={Signup} />
+					<Route exact path="/login" component={Signin} />
+				</Switch>
+
+				<Footer />
 			</BrowserRouter>
 		</div>
 	);
